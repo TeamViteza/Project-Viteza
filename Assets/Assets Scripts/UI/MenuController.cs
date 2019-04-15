f69930eac2e8f42c5e892a288c523bf95d493904
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-04-15 18:19:17 +0100

    Setting up main menu button functionality.

---
+++ Assets/Assets Scripts/UI/MenuController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    Canvas mainCanvas;
+    Transform buttonPositionParent, mainButtonsParent;
+    Transform[] buttonPositions = new Transform[7];
+    Button[] mainButtons = new Button[4];
+
+    void Awake()
+    {
+        mainCanvas = GameObject.Find("canvas_main").GetComponent<Canvas>(); // Get access to the main menu's canvas.
+        buttonPositionParent = mainCanvas.transform.Find("button_positions_main").GetComponent<Transform>(); // Get access to the parent of the main menu's potential button positions.
+        mainButtonsParent = mainCanvas.transform.Find("buttons_main").GetComponent<Transform>(); // Get access to the parent of the main menu's buttons.
+
+        #region Gain access to each of the main menu's potential button positions.
+        for (int i = 0; i < buttonPositions.Length; i++)
+        {
+            buttonPositions[i] = buttonPositionParent.transform.GetChild(i);
+        }
+        #endregion
+
+        #region Now get a reference to each button on the main menu.       
+        for (int i = 0; i < mainButtons.Length; i++) 
+        {
+            mainButtons[i] = mainButtonsParent.transform.GetChild(i).GetComponent<Button>();
+        }
+        #endregion
+    }
+
+    void Start()
+    {       
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }
+    }
+}
