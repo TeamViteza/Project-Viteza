4b2186306d747ce103693bd0f89ccb4969da0a7a
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-04-18 22:14:58 +0100

    Folder structure updated.
    Some files have been renamed also.

--- Assets/Assets Scripts/GUI/Hearts.cs
+++ Assets/Assets Scripts/GUI/Hearts.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Hearths : MonoBehaviour {
+public class Hearts : MonoBehaviour {
 
     public int health;
     public int numberOfHearts;
