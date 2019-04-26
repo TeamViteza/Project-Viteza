bd50533f832477297b416807c84c709a702a2c1a
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-04-26 18:59:23 +0100

    Scrolling background on main menu implemented.

---
+++ Assets/Assets Scripts/UI/ImageScroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageScroller : MonoBehaviour {
+
+    Renderer sRenderer;
+    Image image;
+    public float scrollSpeed = 0.5f;
+	
+	void Start () {
+        sRenderer = GetComponent<Renderer>();
+	}
+		
+	void Update () {
+        Vector2 offset = new Vector2(-Time.time * scrollSpeed, Time.time * scrollSpeed);
+        sRenderer.material.mainTextureOffset = offset;
+	}
+}
