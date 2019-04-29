7a6d795923d9c9660f63c92955143d793e0ce082
Adrian <fearonedm@outlook.com>
2019-04-29 20:25:50 +0100

    Power bar wip

---
+++ Assets/Assets Scripts/GUI/PowerBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBar : MonoBehaviour {
+
+    public float CurrentProg { get; set; }
+    public float MaxProg { get; set; }
+
+    public Image powerMeter;
+
+    // Use this for initialization
+    void Start () {
+        MaxProg = 20f;
+
+        CurrentProg = MaxProg;
+
+        powerMeter.fillAmount = CalulatePower();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            ProgGet(0);
+        }
+	}
+
+    void ProgGet(float progValue)
+    {
+        CurrentProg += progValue;
+        powerMeter.fillAmount = CalulatePower();
+    }
+
+    float CalulatePower()
+    {
+        return CurrentProg / MaxProg;
+    }
+}
