7f1bd4cf4373ffb1477a5894e4388dba3f63f0e8
William McLaughlin <williammclaughlin0@hotmail.com>
2019-05-14 17:34:22 +0100

    Player Movement fixed
    changed the player movement to work off keyboard detection instead of input

---
+++ Assets/Assets Scripts/Player/Movement/scr_DoubleJump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_DoubleJump : MonoBehaviour {
+
+    Rigidbody2D body;
+    BoxCollider2D detectBox;
+
+    float jumpForce = 2f;
+    bool jumpAbility;
+    bool doubleJump = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+        detectBox = GetComponent<BoxCollider2D>();
+	}
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Space) && jumpAbility == true)
+        {
+            body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            jumpAbility = false;
+        }
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            jumpAbility = true;
+        }
+    }
+}
--- Assets/Assets Scripts/Player/Movement/scr_PlayerMovement.cs
+++ Assets/Assets Scripts/Player/Movement/scr_PlayerMovement.cs
@@ -6,74 +6,36 @@ public class scr_PlayerMovement : MonoBehaviour
 {
 
     // Properties
-    // public
-    public Rigidbody2D body;
+    Rigidbody2D body;
 
-    public float jumpForce = 2f;
+    float moveSpeed = 8f;
 
-    public float walkSpeed;
-    public float runSpeed;
-
-    // private
-    private bool jumbAbility;
-
-    private float horizontalMovement;
-
-    private float maxWalk;
-    private float maxRun;
-
-    private float moveSpeed;
-    private float maxSpeed;
+    float horizontalMove;
 
     // Methods
     void Start()
     {
-        maxWalk = 2f;
-        maxRun = 6;
+        body = GetComponent<Rigidbody2D>();
     }
-
-
     void Update()
     {
-        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        horizontalMove = Input.GetAxisRaw("D-PadH");
 
-        body.velocity += new Vector2(horizontalMovement * moveSpeed, 0);
+        body.AddForce(new Vector2(horizontalMove * moveSpeed, 0));
 
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            body.AddForce(new Vector2(-moveSpeed, 0));
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            body.AddForce(new Vector2(moveSpeed, 0));
+        }
     }
     private void FixedUpdate()
     {
-        SpeedManagement();
-
-        if (Input.GetKey(KeyCode.Space) && jumbAbility)
-        {
-            body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            jumbAbility = false;
-        }
-    }
-    void SpeedManagement()
-    {
-        if (body.velocity.x < maxWalk && body.velocity.x > -maxWalk)
-        {
-            moveSpeed = walkSpeed;
-            maxSpeed = maxWalk;
-        }
-        else if (body.velocity.x < maxRun && body.velocity.x > -maxRun)
-        {
-            moveSpeed = runSpeed;
-            maxSpeed = maxRun;
-        }
-        else if (body.velocity.x > maxSpeed && body.velocity.x < -maxRun)
-        {
-            moveSpeed = 0;
-            body.velocity = new Vector2(maxSpeed, body.velocity.y);
-        }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Platform")
-        {
-
-            jumbAbility = true;
-        }
+        Debug.Log(gameObject.transform.position.ToString());
+        Debug.Log(horizontalMove.ToString());
+        Debug.Log(moveSpeed.ToString());
     }
 }
