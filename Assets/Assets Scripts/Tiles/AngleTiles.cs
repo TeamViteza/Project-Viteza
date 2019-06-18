96ede67461ff4afc489ca76c3c2e678cb44396a4
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-06-18 18:12:23 +0100

    Trying to create a scriptable object called "AngleTile".

---
+++ Assets/Assets Scripts/Tiles/AngleTiles.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+//[CreateAssetMenu(fileName = "New AngleTile", menuName = "angleTile")]
+public class AngleTiles : Tile {
+
+    public float slopeAngle;
+	
+}
