dc7b6b5616fd7efdbbf087d84c4511a9eb3b2b04
Adrian <fearonedm@outlook.com>
2019-07-02 16:28:03 +0100

    patrolling wip

---
+++ Assets/Assets Scripts/Enemy/Patorling_Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    public float speed;
+    public Transform[] moveTo;
+    private int randomSpot;
+    private float wait;
+    public float startWait;
+
+    // Use this for initialization
+    void Start()
+    {
+        wait = startWait;
+
+        randomSpot = Random.Range(0, moveTo.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveTo[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveTo[randomSpot].position) < 0.2f)
+        {
+            if (wait <= 0)
+            {
+                randomSpot = Random.Range(0, moveTo.Length);
+                wait = startWait;
+            }
+            else
+            {
+                wait -= Time.deltaTime;
+            }
+        }
+    }
+}
