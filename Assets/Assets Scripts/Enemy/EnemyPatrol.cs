0c41fe4c7f55e6301c1b841fc16683b6c2be4d94
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-07-02 16:37:46 +0100

    Changed enemy patrol script name.

--- Assets/Assets Scripts/Enemy/EnemyPatrol.cs
+++ Assets/Assets Scripts/Enemy/EnemyPatrol.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-
     public float speed;
     public Transform[] moveTo;
     private int randomSpot;
     private float wait;
     public float startWait;
 
-    // Use this for initialization
     void Start()
     {
         wait = startWait;
 
         randomSpot = Random.Range(0, moveTo.Length);
     }
-
-    // Update is called once per frame
+ 
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, moveTo[randomSpot].position, speed * Time.deltaTime);
