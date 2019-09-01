af59b7c587f1eb789520035edc683f037b0917d7
Jack Gilmartin <s00171013@mail.itsligo.ie>
2019-09-02 00:36:21 +0100

    Spring functionality implemented.
    Spike hazards are next.

---
+++ Assets/Assets Scripts/Props/Spring.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour {
+    public int springForce = 15;
+    bool pressed;
+
+	void Start () {
+        pressed = false;
+	}
+		
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Rigidbody2D playerBody = collision.gameObject.GetComponent<Rigidbody2D>();
+            playerBody.AddForce(new Vector2(0, springForce), ForceMode2D.Impulse);          
+        }
+    }
+}
